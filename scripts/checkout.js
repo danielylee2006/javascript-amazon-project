56307143743ef83e7bdef55d43ac521ba8b6db38
Daniel Lee <danielyedarmlee@gmail.com>
2025-07-05 19:59:30 -0400

    create loadPage function using async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,17 +5,44 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend.js';
 
+async function loadPage() {
+  //async keyword makes function return a promise
+
+  await loadProductsFetch(); //makes code wait for async function to finish.
+
+  /*
+  await can only be used if function returns promise. 
+  Although loadCart() is an async function, it does not return 
+  a promise. So we wrap it with a promise and add await.
+  */
+  
+  const variable = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value'); // 'value' gets saved in variable
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+
+  return "value"; // equivalent to resolve('value')
+}
+loadPage();
+
+
+/* NONE ASYNC AWAIT APPROACH USING ONLY PROMISES!!!!
+
 //running multiple promises at the same time and waiting for all of them to finish
 Promise.all([
+  loadProductsFetch(),
 
- loadProductsFetch(),
- 
   new Promise((resolve) => {
     loadCart(() => {
       resolve("hello");
     });
   }),
-]).then((values) => { //values -> ['hi', 'hello']
+]).then((values) => {
+  //values -> ['hi', 'hello']
   renderOrderSummary();
   renderPaymentSummary();
 });
@@ -24,3 +51,5 @@ Promise.all([
 Promise is used for more efficient call backs. Multiple call backs lead to nesting
 of functions. But with an Promise Object, we can use .then() for easier call back managment. 
 */
+
+
